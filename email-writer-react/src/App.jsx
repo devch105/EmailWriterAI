8c448ac5f51c1836e6cec6db5da5d1ce63f22fa3
devch105 <you@gmail.com>
2025-10-20 14:35:15 +0530

    url changes

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -1,5 +1,5 @@
 // top of file
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE = "https://quickmailv1.onrender.com";
 
 import { useState } from "react";
 import "./App.css";
